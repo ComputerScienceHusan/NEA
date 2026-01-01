81ca48a4bb567eb3fa04a4a99588fe7458b77a77
ComputerScienceHusan <1husan.ahmed1@gmail.com>
2026-01-01 09:11:14 +0000

    Game interface

---
+++ Boundless Runner backup.py
@@ -0,0 +1,38 @@
+import sys
+import pygame
+
+class Game:
+    def __init__(self):
+        pygame.init()
+
+        pygame.display.set_caption("Boundless Runner")
+        self.screen = pygame.display.set_mode((640, 480))
+
+        self.clock = pygame.time.Clock()
+
+
+        self.movement = [False, False]
+
+
+    def run(self):
+        while True:
+            self.screen.fill((14, 219, 248))
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        self.movement[0] = True
+                    if event.key == pygame.K_RIGHT:
+                        self.movement[1] = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_LEFT:
+                        self.movement[0] = False
+                    if event.key == pygame.K_RIGHT:
+                        self.movement[1] = False
+
+            pygame.display.update()
+            self.clock.tick(60)
+
+
